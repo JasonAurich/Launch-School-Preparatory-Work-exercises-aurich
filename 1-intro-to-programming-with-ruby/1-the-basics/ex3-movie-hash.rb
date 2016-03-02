a8d4d205a56229abf6a349762c739db96eaeb389
Jason Aurich <jason@refund-retriever.com>
2016-03-02 02:45:07 -0600

    Completes chapter 1/exercise 3

---
+++ 1-intro-to-programming-with-ruby/1-the-basics/ex3-movie-hash.rb
@@ -0,0 +1,13 @@
+movielist = {
+    the_godfather:    1972,
+    the_matrix:           1999,
+    star_wars:             1977,
+    fight_club:           1999,
+    idiocracy:              2006
+}
+
+p movielist[:the_godfather]
+p movielist[:the_matrix]
+p movielist[:star_wars]
+p movielist[:fight_club]
+p movielist[:idiocracy]
