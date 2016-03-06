0397bc3b557c123df97d25a8e49d6a8dedc301ba
Jason Aurich <jason@refund-retriever.com>
2016-03-06 17:12:55 -0600

    completes ch 2 / ex 4

--- 1-intro-to-programming-with-ruby/2-variables/exercises-1-4/name.rb
+++ 1-intro-to-programming-with-ruby/2-variables/exercises-1-4/name.rb
@@ -1,13 +1,15 @@
 puts "\e[H\e[2J"
-puts "Hi. What's your name?"
-name = gets.chomp
-puts "Hello, " + name + "! I'm so glad you could be here!"
+puts "Hi. What's your first name?"
+first_name = gets.chomp
+puts "And your last name?"
+last_name = gets.chomp
+puts "Hello, " + first_name + ' ' + last_name + "! I'm so glad you could be here!"
 
 10.times do |n|
     a = n + 1
     if a < 10
-        puts a.to_s + '   ' + name
+        puts a.to_s + '   ' + first_name + ' ' + last_name
     else
-        puts a.to_s + '  ' + name
+        puts a.to_s + '  ' + first_name + ' ' + last_name
     end
 end
