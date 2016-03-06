336f1d56726739adcf2f6b610e293bdf29f38f3c
Jason Aurich <jason@refund-retriever.com>
2016-03-06 16:53:37 -0600

    Updates var in ch 2 / ex 2

--- 1-intro-to-programming-with-ruby/2-variables/exercise-2/age.rb
+++ 1-intro-to-programming-with-ruby/2-variables/exercise-2/age.rb
@@ -3,7 +3,7 @@ puts "Hi. May I ask your age, please?"
 
 4.times do |n|
     i = (n + 1) * 10
-    f_age = @age + i
+    future_age = @age + i
     puts
-    puts 'In ' + i.to_s + ' years, you will be ' + f_age.to_s + ' .'
+    puts 'In ' + i.to_s + ' years, you will be ' + future_age.to_s + ' .'
 end
