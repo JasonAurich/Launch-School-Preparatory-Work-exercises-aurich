a1b9b97c781fb79cbae457565cf092ab2abadf8b
Jason Aurich <jason@refund-retriever.com>
2016-03-06 19:31:05 -0600

    Completes ch 3 / ex 2

---
+++ 1-intro-to-programming-with-ruby/3-methods/ex-2/ex-2.rb
@@ -0,0 +1,9 @@
+2.2.2 :001 > x = 2                                                      # => 2
+
+2.2.2 :002 > puts x = 2                                             # => nil
+
+2.2.2 :003 > p name = "Joe"                                  # => "Joe"
+
+2.2.2 :004 > four = "four"                                        # => "four"
+
+2.2.2 :005 > print something = "nothing"            # nothing => nil
