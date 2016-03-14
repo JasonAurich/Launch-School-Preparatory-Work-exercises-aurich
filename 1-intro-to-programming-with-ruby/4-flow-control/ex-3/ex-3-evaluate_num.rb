16e9cb6dfbcb420f59e1652780f076d71ef12b52
Jason Aurich <jason@refund-retriever.com>
2016-03-13 22:57:04 -0500

    Updates ch 4 / ex 3

--- 1-intro-to-programming-with-ruby/4-flow-control/ex-3/ex-3-evaluate_num.rb
+++ 1-intro-to-programming-with-ruby/4-flow-control/ex-3/ex-3-evaluate_num.rb
@@ -1,15 +1,13 @@
     puts "\e[H\e[2J"
     puts '',"Give me a number between 0 and 100, and I'll tell you if it's between 0 and 50, 51 and 100, or above 100."
-    num = gets.to_i
+    get_num = gets.to_i
 
     if num < 0
-        puts '',"#{num} a negative number!"
+        puts '',"#{num} is too small!  Can't you follow instructions?"
     elsif num <= 50
         puts '',"#{num} falls in between 0 and 50!"
     elsif num <= 100
         puts '',"#{num} falls in between 51 and 100!"
-    elsif num < 1
-        puts '',"#{num} is too small!  Can't you follow instructions?"
     else
         puts '',"#{num} is too big!  Can't you follow instructions?"
     end
