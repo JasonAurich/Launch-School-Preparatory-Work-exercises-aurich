53648d03ec25e5b902227fc9ecdebdc16c2e3927
Jason Aurich <jason@refund-retriever.com>
2016-04-04 01:21:56 -0500

    Completes book ex 10

---
+++ 1-intro-to-programming-with-ruby/10-book-exercises/ex-10/ex-10-nested.rb
@@ -0,0 +1,62 @@
+# array of hashes
+abc1 = [
+  {aleph:  "alpha"},
+  {beit:   "beta"},
+  {gimmel: "gamma"},
+  {dalet:  "delta"},
+  {he:     "eta"},
+  {zayin:  "zeta"},
+  {khet:   "chi"},
+  {tet:    "theta"},
+  {yod:    "iota"},
+  {kaph:   "kappa"},
+  {lamed:  "lamda"},
+  {mem:    "mu"},
+  {nun:    "nu"},
+  {pe:     "pi"},
+  {resh:   "rho"},
+  {samekh: "sigma"},
+  {tau:    "tau"}
+]
+
+# hash of arrays
+abc2 = {
+  hebrew: [
+    'aleph',
+    'beit',
+    'gimmel',
+    'dalet',
+    'he',
+    'zayin',
+    'khet',
+    'tet',
+    'yod',
+    'kaph',
+    'lamed',
+    'mem',
+    'nun',
+    'pe',
+    'resh',
+    'samekh',
+    'tau'
+  ],
+  greek:  [
+    'alpha',
+    'beta',
+    'gamma',
+    'delta',
+    'eta',
+    'zeta',
+    'chi',
+    'theta',
+    'iota',
+    'kappa',
+    'lamda',
+    'mu',
+    'nu',
+    'pi',
+    'rho',
+    'sigma',
+    'tau'
+  ]
+}
