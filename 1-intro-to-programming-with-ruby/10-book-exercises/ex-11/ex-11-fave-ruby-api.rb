54b7474dda4e850eb663abcd4495770d5a0baa88
Jason Aurich <jason@refund-retriever.com>
2016-04-07 15:23:56 -0500

    Completes book ex 11

---
+++ 1-intro-to-programming-with-ruby/10-book-exercises/ex-11/ex-11-fave-ruby-api.rb
@@ -0,0 +1,3 @@
+fave_ruby_api = "Typically, I just use http://ruby-doc.org/ or http://stackoverflow.com/. (Yes, I know Stack isn't an API, but it gets the job done.) The very simple reason for this is that they are usually at the top of the list of my Google search results. Though, at times, I will take a look at http://guides.rubyonrails.org/"
+
+p fave_ruby_api
